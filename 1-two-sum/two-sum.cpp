48db19e94b82a2573d67dc31b5c1be9f93a0ef8e
Ishaan Jain <143131679+ishaanj26@users.noreply.github.com>
2025-03-20 02:25:05 +0530

    Time: 3 ms (73.19%) | Memory: 15 MB (21.30%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen; // Map to store the number and its index
-
-        // Loop through the list of numbers
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i]; // Calculate the complement
-
-            // Check if the complement is already in the map
-            if (seen.find(complement) != seen.end()) {
-                // If found, return the indices of the two numbers
-                return {seen[complement], i};
-            }
-
-            // Otherwise, store the current number and its index in the map
-            seen[nums[i]] = i;
+      unordered_map<int,int>seen;//create a hash table where key is comeplement and value is index
+      for(int i=0;i<nums.size();i++){ //run a O(n) loop
+        int complement = target - nums[i]; //check if complement exist 
+        if(seen.find(complement)!=seen.end()){ 
+            //if complement exist,we return the the complement index and the current value index
+            return {seen[complement],i};
         }
-
-        // Return an empty vector if no solution is found (optional, based on
-        // the problem statement)
-        return {};
+        seen[nums[i]]=i; 
+        //else we add the value to the has table which can be check as complement later on
+      }
+      return {};
+      //if no exist we return as empty array
     }
 };
